7e1da27907d12f97c598353fc0eac59f8b008848
Matthew Spence <mspence5555@gmail.com>
2022-03-10 17:58:53 -0600

    buld files to /backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,7 +13,9 @@ const allowRequest = app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.get('/', function (req,res) {
+    res.sendFile("index.html");
+});
 router.post('/getmenu', allowRequest, index.getMenu);
 
 router.post('/create-payment-intent', allowRequest, payment.paymentIntent)
